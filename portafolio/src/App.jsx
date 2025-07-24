600219b858a57ad800fe00408e4e93f1aab79ff6
Ordogen <oarcilagarcia@gmail.com>
2025-07-23 22:06:27 -0500

    creacion del header

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Header } from './Header/Header'
 
 export const App = () => {
  
 
   return (
     <div className='App'>
+      <Header />
     
     </div>
   )
---
+++ portafolio/src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css"
+
+export const Header = () => {
+  return (
+    <div className="Header">
+        <ul>
+            <li className="rojo"></li>
+            <li className="amarillo"></li>
+            <li className="verde"></li>
+        </ul>
+    </div>
+  )
+}
